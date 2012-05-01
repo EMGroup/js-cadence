39a6af9d3d5b20303c75598e2c133351ac560b06
Jseden Account <jseden@dcs.warwick.ac.uk>
2012-05-01 14:14:13 +0100

    Deleted old cadence files

--- service/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-g++ -o service.bin service.cpp tinystr.cpp tinyxml.cpp tinyxmlerror.cpp tinyxmlparser.cpp protocol.cpp graphdb.cpp client.cpp observerdb.cpp -ldb
-cp ./service.bin ../
-chmod a+rx ../service.bin
